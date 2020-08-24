6b0549988c14d02fefb7e3a50c0272f8e146c706
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-24 11:49:11 +0800

    建立資料庫_OK

--- db.sql
+++ db.sql
@@ -1,13 +1,14 @@
-create database test;
+create database test1;
 
-use test;
+use test1;
 
 CREATE TABLE `user` (
   `id` int NOT NULL auto_increment primary key,
   `username` varchar(20) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `password` varchar(30) NOT NULL,
+  `cash` int default 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `user` (`id`,`username`,`password`) VALUES
-(1,'aaa','111'),
-(2,'bbb','222');
+INSERT INTO `user` (`id`,`username`,`password`,`cash`) VALUES
+(1,'aaa','111',111),
+(2,'bbb','222',0);
