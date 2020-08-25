d2591d36202e756b53e4dce08982d4f174d844cb
alanwang1207 <alan_wang@mail.chungyo.net>
2020-08-25 08:00:58 +0800

    建立明細表

--- db.sql
+++ db.sql
@@ -9,6 +9,16 @@ CREATE TABLE `user` (
   `cash` int default 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `detail`(
+  `did` int NOT NULL auto_increment primary key,
+  `uid` int not null,
+  `cash` int not null,
+  `decash` int default 0,
+  `dcash` int default 0,
+  FOREIGN KEY (`uid`) REFERENCES user(`id`),
+  FOREIGN KEY (`cash`) REFERENCES user(`cash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 INSERT INTO `user` (`id`,`username`,`password`,`cash`) VALUES
 (1,'aaa','111',111),
 (2,'bbb','222',0);
