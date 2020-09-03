57a2dfe1bc089e58498e9defce9717ec889da132
alanwang1207 <alan_wang@mail.chungyo.net>
2020-09-03 09:15:49 +0800

    修改資料庫_ok

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
-create database test2;
+create database rd5;
 
-use test2;
+use rd5;
 
 CREATE TABLE `user` (
   `id` int NOT NULL auto_increment primary key,
@@ -22,21 +22,3 @@ CREATE TABLE `detail`(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `user` (`id`,`username`,`password`,`tid`,`email`,`cash`) VALUES
-(1,'aaa','111','A123456789','aaa111@gmail.com',111),
-(2,'bbb','222','B123456789','bbb222@gmail.com',0);
-
-
-INSERT INTO `detail` (`uid`,`decash`,`dcash`,`cash`,`date`) VALUES
-(1,100,0,current_timestamp()),
-(1,100,0,current_timestamp()),
-(1,100,0,current_timestamp()),
-(2,0,200,current_timestamp()),
-(2,50,0,current_timestamp()),
-(2,0,100,current_timestamp());
-
-
-
-select decash,dcash,cash,date from detail d join user u on d.uid = u.id where uid = 1
-
-select cid,username,dis from customer c join block_list b on c.cid = b.bid ;
