6ac167e3029020d4c98e477770654d1b1cd5b180
alanwang1207 <alan_wang@mail.chungyo.net>
2020-09-03 10:13:09 +0800

    刪除會員debug

--- delete.php
+++ delete.php
@@ -1,15 +1,29 @@
 <?php
+//引入資料庫配置
+require("config.php");
 
+//檢查是否有id
 if (!isset($_GET["id"])) {
   die("id not found");
 }
 $id = $_GET["id"];
+
+//判斷id是否為數字或數字的字串
 if (!is_numeric($id))
   die("id not a number.");
+
+//刪除明細
+$sql = <<<multi
+  delete from detail where uid = $id
+multi;
+mysqli_query($link, $sql);
+
+//刪除user
 $sql = <<<multi
   delete from user where id = $id
 multi;
-require("config.php");
 mysqli_query($link, $sql);
+
+
 echo "<script> alert('刪除成功，將跳回登入頁');location.replace('login.php');</script>";
-// header("location: login.php");
+
