8a732a5850af0832a8e86e59d8b784103aec03da
alanwang1207 <alan_wang@mail.chungyo.net>
2020-09-07 12:18:02 +0800

    修改樣式

--- index.php
+++ index.php
@@ -21,13 +21,15 @@ if (isset($_SESSION["userName"])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 
 <body>
 
     <div class="container">
-        <h2>線上網銀系統 - 首頁</h2>
-        <p><td align="center" bgcolor="#CCCCCC"><?php echo "Hello~ " . $sUserName ?> </td></p>
+        <h2 >線上網銀系統 - 首頁</h2>
+        <p><td id="tt"><?php echo "Hello~ " . $sUserName ?> </td></p>
             
         <span>
             
